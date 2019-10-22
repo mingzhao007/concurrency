c6fc601b4a3b9c1defa07f69d75f991f4aeee416
mingzhao007 <1137386887@qq.com>
2019-10-22 22:47:49 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/NotRecommend.java
@@ -0,0 +1,16 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程里用来标记【推荐】的类或者写法
+ * Created by HUAWEI on 2019/10/22.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)//注解存在的范围，source编译的时候就会被忽略掉
+public @interface NotRecommend {
+    String value() default "";
+}
